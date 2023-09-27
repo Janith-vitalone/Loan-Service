5832cf9f43a189af3f258c6c65fdf84a392ab0b5
Janiya-99 <janithsamarasinghe1999@gmail.com>
2023-09-27 14:47:51 +0530

    Added PDC security form

---
+++ storage/framework/views/027e1fadbf513fe907849f9e6e45ad4a.php
@@ -0,0 +1,79 @@
+<?php $__env->startSection('title'); ?>
+    <?php echo app('translator')->get('translation.input-masks'); ?>
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title mb-0">PDC Security For Business Loan Daily</h4>
+                </div><!-- end card header -->
+                <div class="card-body p-5">
+                    <form action="">
+                        <div class="col-md-6">
+                            <label for="employeeName" class="form-label">Product Daily</label>
+                            <select class="form-select mb-3" aria-label=".form-select-lg example">
+                                <option selected>Business Loan - Daily</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="validationDefault03" class="form-label">Contract Code</label>
+                            <input type="text" class="form-control" id="validationDefault03" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="employeeName" class="form-label">VILLAGE</label>
+                            <div class="row">
+                                <div class="col-12 mt-2">
+                                    <select class="form-select mb-3" aria-label=".form-select-lg example">
+                                        <option selected>Select Branch CODE</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;
+
+                                    <select class="form-select mb-3" aria-label=".form-select-lg example">
+                                        <option selected>Select Branch CODE</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="employeeName" class="form-label">CRO</label>
+                            <select class="form-select mb-3" aria-label=".form-select-lg example">
+                                <option selected>Select Branch CODE</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="validationDefault03" class="form-label">ROOT NAME</label>
+                            <input type="text" class="form-control" id="validationDefault03" required disabled>
+                        </div>
+                        <div class="text-end">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div><!-- end card-body -->
+            </div><!-- end card -->
+        </div>
+        <!-- end col -->
+    </div>
+    <!-- end row -->
+
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('script'); ?>
+    <script src="<?php echo e(URL::asset('build/libs/cleave.js/cleave.min.js')); ?>"></script>
+    <script src="<?php echo e(URL::asset('build/js/pages/form-masks.init.js')); ?>"></script>
+    <script src="<?php echo e(URL::asset('build/js/app.js')); ?>"></script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.sales layout.layouts-detached', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/Loan_serv_L/resources/views/sales/pdc-security-form.blade.php ENDPATH**/ ?>
